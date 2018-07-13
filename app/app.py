a786fbd33b10b6764dfb271e4d60737b135c9033
Hassan Yahya Hussein <hassan@localhost.localdomain>
2018-07-13 17:17:37 +0300

    [CHORE]Add create_app

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restplus import Api
+from configurations.config import app_config
+
+api = Api(
+    version="1.0",
+    title="MY DIARY API",
+    description="A simple My Diary API",
+    prefix='/api/v1')
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+    api.init_app(app)
+    return app
