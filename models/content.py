e1cba39ef03d3a4e0214a211db729bfe6550057c
Hassan Yahya Hussein <husain.host@gmail.com>
2018-07-15 16:34:35 +0300

    [CHORE]Add an COntentID to a entry model

--- models/content.py
+++ models/content.py
@@ -3,11 +3,17 @@ content_data = []
 
 class Content:
     """Entry of user in diary"""
+    contentID = 0
 
     def __init__(self, date_created, content):
         self.date_created = date_created
         self.content = content
 
     def create(self):
-        my_diary = {"Date": self.date_created, "Content": self.content}
+        my_diary = {
+            "ContentID": Content.contentID,
+            "Date": self.date_created,
+            "Content": self.content
+        }
+        Content.contentID += 1
         return content_data.append(my_diary)
