2cabfe68f1b75a6e029eb868bae54c2ef7964228
Yahya Hussein <40847561+YA7YA-H@users.noreply.github.com>
2018-07-16 22:45:41 +0300

    Revert "#159057280- Api login"

--- endpoints/user.py
+++ endpoints/user.py
@@ -1,6 +1,5 @@
 from flask_restplus import Resource, Namespace, fields
 from flask import request
-from flask_bcrypt import Bcrypt
 from models.user import user_data, User
 
 auth_namespace = Namespace(
@@ -14,20 +13,6 @@ registration_model = auth_namespace.model(
         "LastName":
         fields.String(
             required=True, description='Your Last Name', example='Doe'),
-        "Email":
-        fields.String(
-            required=True,
-            description='your email accounts',
-            example='John_Doe@example.com'),
-        "Password":
-        fields.String(
-            required=True,
-            description='Your secret password',
-            example='its26uv3nf')
-    })
-
-login_model = auth_namespace.model(
-    'Login', {
         "Email":
         fields.String(
             required=True,
@@ -62,35 +47,3 @@ class Signup(Resource):
             'status': 'success',
             'message': 'Successfully registered.'
         }, 201
-
-
-@auth_namespace.route('/login')
-@auth_namespace.doc(
-    responses={
-        201: 'Successfully login',
-        401: 'Invalid credential'
-    },
-    security=None,
-    body=login_model)
-class Login(Resource):
-    """Handles api registration url api/auth/signin."""
-
-    def post(self):
-        """Handle POST request for login"""
-        post = request.get_json()
-        user_email = post['Email']
-        user_password = post['Password']
-
-        if user_email in user_data:
-            if Bcrypt().check_password_hash(user_data[user_email]["Password"],
-                                            user_password):
-                return {
-                    'status': 'success',
-                    'message': 'Successfully login.'
-                }, 201
-            else:
-                return {
-                    "message": "Failed, Invalid password! Please try again"
-                }, 401
-        else:
-            return {"message": "Failed, Invalid email! Please try again"}, 401
--- test/test_auth.py
+++ test/test_auth.py
@@ -12,76 +12,21 @@ class EntryTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client()
-        self.user_registration = {
+        self.user = {
             "FirstName": "John",
             "LastName": "Doe",
-            "Email": "John_Doe@example.com",
-            "Password": "its26uv3nf"
-        }
-        self.user_login = {
             "Email": "John_Doe@example.com",
             "Password": "its26uv3nf"
         }
 
     def test_user_signup(self):
         """Test user registration"""
+
         response = self.client.post(
             '/api/v1/auth/signup',
-            data=json.dumps(self.user_registration),
+            data=json.dumps(self.user),
             content_type="application/json")
+
         result = json.loads(response.data)
         self.assertEqual(result['message'], 'Successfully registered.')
         self.assertEqual(response.status_code, 201)
-
-    def test_api_user_login_successfully(self):
-        """Test user signin successfully"""
-        response = self.client.post(
-            '/api/v1/auth/signup',
-            data=json.dumps(self.user_registration),
-            content_type="application/json")
-        self.assertEqual(response.status_code, 201)
-        response = self.client.post(
-            '/api/v1/auth/login',
-            data=json.dumps(self.user_login),
-            content_type="application/json")
-        result = json.loads(response.data)
-        self.assertEqual(result['message'], 'Successfully login.')
-        self.assertEqual(response.status_code, 201)
-
-    def test_api_invalid_email(self):
-        """Test for invalid email in signin endpoint"""
-        response = self.client.post(
-            '/api/v1/auth/signup',
-            data=json.dumps(self.user_registration),
-            content_type="application/json")
-        self.assertEqual(response.status_code, 201)
-        response = self.client.post(
-            '/api/v1/auth/login',
-            data=json.dumps({
-                "Email": "John@example.com",
-                "Password": "its26uv3nf"
-            }),
-            content_type='application/json')
-        result = json.loads(response.data)
-        self.assertEqual(result['message'],
-                         'Failed, Invalid email! Please try again')
-        self.assertEqual(response.status_code, 401)
-
-    def test_api_invalid_password(self):
-        """Test for invalid password in signin endpoint"""
-        response = self.client.post(
-            '/api/v1/auth/signup',
-            data=json.dumps(self.user_registration),
-            content_type="application/json")
-        self.assertEqual(response.status_code, 201)
-        response = self.client.post(
-            '/api/v1/auth/login',
-            data=json.dumps({
-                "Email": "John_Doe@example.com",
-                "Password": "fakepassword"
-            }),
-            content_type='application/json')
-        result = json.loads(response.data)
-        self.assertEqual(result['message'],
-                         'Failed, Invalid password! Please try again')
-        self.assertEqual(response.status_code, 401)
