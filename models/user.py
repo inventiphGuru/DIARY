82c9154dc1e5d9a62dcffc89e30e1c58455f26a8
Hassan Yahya Hussein <husain.host@gmail.com>
2018-07-17 08:12:45 +0300

    [CHORE]-#159089754]Add encode code in user model

--- models/user.py
+++ models/user.py
@@ -23,3 +23,23 @@ class User:
             },
         }
         return user_data.update(user)
+
+    @classmethod
+    def encode_auth_token(cls, user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+
+        try:
+            payload = {
+                'exp':
+                datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+                'iat':
+                datetime.datetime.utcnow(),
+                'sub':
+                user_id
+            }
+            return jwt.encode(payload, os.getenv('SECRET'), algorithm='HS256')
+        except Exception as e:
+            return e
