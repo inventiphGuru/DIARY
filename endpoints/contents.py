ea31010dc59aab87a2dae66ce1d34aeb13485e4c
Hassan Yahya Hussein <husain.host@gmail.com>
2018-07-17 20:46:57 +0300

    [FEATURE]-#159089754]Add token required for all endpoints .

--- endpoints/contents.py
+++ endpoints/contents.py
@@ -2,6 +2,7 @@
 from flask_restplus import Resource, Namespace, fields
 from models.content import content_data, Content
 from flask import request
+from models.token import token_required
 
 entries_namespace = Namespace("User", description="Content related endpoints")
 entries_model = entries_namespace.model(
@@ -20,14 +21,17 @@ entries_model = entries_namespace.model(
 
 
 @entries_namespace.route("/entries")
-@entries_namespace.doc(responses={201: "Entry successfully created"})
+@entries_namespace.doc(
+    responses={201: "Entry successfully created"}, security="apikey")
 class UserEntry(Resource):
     """This class handles get requests in user entry endpoint"""
 
+    @token_required
     def get(self):
         """Handle get request of url /entries"""
         return content_data
 
+    @token_required
     @entries_namespace.expect(entries_model)
     def post(self):
         """Handle post request of url/entries"""
@@ -45,10 +49,12 @@ class UserEntry(Resource):
         201: "Entry successfully updated",
         400: "Invalid parameters provided",
         404: "Entry not found"
-    })
+    },
+    security="apikey")
 class UpdateEntry(Resource):
     """Handle [UPDATE] request of URL user/entries/id"""
 
+    @token_required
     def get(self, contentID):
         an_update = [
             result for result in content_data
@@ -58,6 +64,7 @@ class UpdateEntry(Resource):
             return {'Status': "No entry found"}, 404
         return an_update
 
+    @token_required
     @entries_namespace.expect(entries_model)
     def put(self, contentID):
         """Modify a entries."""
@@ -74,6 +81,7 @@ class UpdateEntry(Resource):
 
             return {"status": " Entry content successfully created"}, 201
 
+    @token_required
     def delete(self, contentID):
         del_item = [
             del_item for del_item in content_data
