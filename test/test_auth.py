247a1416aa618c2cfdd15a6a2f9359169ee6ee2c
Hassan Yahya Hussein <husain.host@gmail.com>
2018-07-18 13:10:52 +0300

    [CHORE]-#159057283].

--- test/test_auth.py
+++ test/test_auth.py
@@ -6,10 +6,10 @@ from app.app import create_app
 from models.user import User
 import inspect
 import sys
-currentdir = os.path.dirname(os.path.abspath(
-    inspect.getfile(inspect.currentframe())))
+currentdir = os.path.dirname(
+    os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir
+sys.path.insert(0, parentdir)
 
 
 class EntryTestCase(unittest.TestCase):
